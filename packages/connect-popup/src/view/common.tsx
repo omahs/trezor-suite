8ce24501108c801670eb9e2605a2fe34f6dbef1f
Martin Varmuza <mvarmuza@gmail.com>
2023-07-12 10:25:52 +0200

    fix: popup and webusb

--- packages/connect-popup/src/view/common.tsx
+++ packages/connect-popup/src/view/common.tsx
@@ -122,6 +122,10 @@ export const initMessageChannel = async (
             new Promise<boolean>(resolve => setTimeout(() => resolve(false), 1000)),
         ]);
 
+    const iframe = getIframeElement();
+    if (!iframe) {
+        throw ERRORS.TypedError('Popup_ConnectionMissing');
+    }
     // iframe requested communication via BroadcastChannel.
     if (broadcastId) {
         try {
@@ -137,7 +141,7 @@ export const initMessageChannel = async (
 
             // POPUP.HANDSHAKE successfully received back from the iframe
             if (await broadcastHandshake) {
-                setState({ broadcast, systemInfo });
+                setState({ broadcast, systemInfo, iframe });
                 return;
             }
 
@@ -147,11 +151,6 @@ export const initMessageChannel = async (
         } catch (error) {
             // silent error. use MessageChannel as fallback communication
         }
-    }
-
-    const iframe = getIframeElement();
-    if (!iframe) {
-        throw ERRORS.TypedError('Popup_ConnectionMissing');
     }
 
     // create MessageChannel and assign message listener

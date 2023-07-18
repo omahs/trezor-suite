10ff3e0b1ff7ae0c4ac1515e3dfd9d4943356986
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-07-18 13:53:38 +0200

    fixup! feat(connect-iframe): initialize logWorker

--- packages/connect-iframe/src/logWriter.ts
+++ packages/connect-iframe/src/logWriter.ts
@@ -1,10 +1,38 @@
 // @ts-expect-error (typescript does not know this is worker constructor, this is done by webpack)
 import LogWorker from './sharedLoggerWorker';
-import { LogWriter } from '@trezor/connect/src/utils/debug';
+import { LogMessage, LogWriter } from '@trezor/connect/src/utils/debug';
 
 const logWorker = new LogWorker();
 logWorker.port.start();
 
+const stringify = (obj: Record<string, any>) => {
+    let cache: string[] = [];
+    const str = JSON.stringify(obj, (_key, value) => {
+        if (typeof value === 'object' && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+                // Circular reference found, discard key
+                return;
+            }
+            // Store value in our collection
+            cache.push(value);
+        }
+        return value;
+    });
+    cache = [];
+    return str;
+};
+
+const prepareMessage = (message: LogMessage) => {
+    const { level, prefix, timestamp, css, ...rest } = message;
+    return {
+        level,
+        prefix,
+        timestamp,
+        message: JSON.parse(stringify(rest)),
+    };
+};
+
 export const logWriter = (): LogWriter => ({
-    add: (message: object) => logWorker.port.postMessage({ type: 'add-log', data: message }),
+    add: (message: LogMessage) =>
+        logWorker.port.postMessage({ type: 'add-log', data: prepareMessage(message) }),
 });
